b1a9e8d82a42b185ae15f428673cb3cfbdc7e3f4
emir-canswe <canemircan38@gmail.com>
2025-06-29 15:13:19 +0300

    Update and rename kuyruk to kurukk/mainkismi.java

---
+++ kurukk/mainkismi.java
@@ -0,0 +1,24 @@
+
+package yıgıt_kuyruk;
+
+import classs.claass;
+
+public class maiin {
+
+	public static void main(String[] args) {
+		claass d=new claass(5);
+		
+		d.push(23);//ekleme işlemleri
+		d.push(2);
+		d.push(2233);
+		d.push(22342);
+		d.yazma();
+		d.yazma();
+		d.pop();
+	int a=d.pop();//cikarilan son işlemi main ile disariya aktarma işlemi
+	d.yazma();
+	System.out.println("dizinin ilk elemani :"+a);
+
+	}
+
+}

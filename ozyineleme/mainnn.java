a03db3428be271dccf8deeba71bf3301059d475c
emir-canswe <canemircan38@gmail.com>
2025-07-22 13:38:36 +0300

    Create mainnn.java

---
+++ ozyineleme/mainnn.java
@@ -0,0 +1,17 @@
+package ozyimeleme;
+
+import classlar.clas1;
+
+public class miann {
+
+	public static void main(String[] args) {
+		clas1 a=new clas1();
+		
+		int result=a.test(9);
+		
+		
+		System.out.println(result);
+
+	}
+
+}

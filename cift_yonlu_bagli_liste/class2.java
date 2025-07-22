8a819da748315dc36310067688558b8dbb04345f
emir-canswe <canemircan38@gmail.com>
2025-07-22 12:10:35 +0300

    Create class2.java

---
+++ cift_yonlu_bagli_liste/class2.java
@@ -0,0 +1,15 @@
+package classs;
+
+// Bu sınıf bir düğümü (node) temsil eder.
+public class classass2 {
+    int data; // Düğümde tutulacak veri
+    classass2 next; // Sonraki düğüm bağlantısı
+    classass2 prev; // Önceki düğüm bağlantısı
+
+    // Constructor: veri parametresiyle düğüm oluşturulur
+    public classass2(int d) {
+        data = d;
+        next = null;
+        prev = null;
+    }
+}

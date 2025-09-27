2025dbd585ef522b6be8bad094e37adc2f6784f4
Emircan <canemircan38@gmail.com>
2025-09-27 18:53:32 +0300

    Update classsss.java

--- eklemeli_siralama/classsss.java
+++ eklemeli_siralama/classsss.java
@@ -1,39 +1,29 @@
-package eklemeli_siralama; // Bu sınıfın bulunduğu paket adı
+package eklemekli_siralama;
 
-import java.util.Arrays; // Arrays kütüphanesini içe aktarır (dizi yazdırmak için kullanılır)
+import java.util.Arrays;
 
-public class classsss { // classsss adında bir sınıf tanımlanıyor
-
-	int[] dizi; // Sıralanacak diziyi tutan değişken
-	int n;      // Dizinin uzunluğu (eleman sayısı)
-
-	// Constructor: Sınıf örneği oluşturulurken dizi parametresi alınır
-	public classsss(int[] d) {
-		dizi = d;       // Parametre olarak gelen dizi, sınıf içindeki dizi değişkenine atanır
-		n = d.length;   // Dizinin uzunluğu 'n' değişkenine atanır
+public class clas {
+	int [] array;
+	
+	public clas(int [] dizi) {
+		array=dizi;
 	}
-
-	// Ekleme sıralaması (Insertion Sort) algoritmasını uygular
-	public void ekleme() {
-		int t, j; // t: geçici olarak elemanı tutar, j: sıralama için pozisyon belirler
-
-		// 1. elemandan itibaren tüm elemanlar sıralanır (çünkü ilk eleman zaten sıralıdır)
-		for (int i = 1; i < n; i++) {
-			t = dizi[i]; // Sıradaki elemanı t’ye al
-			j = i;       // j'yi i konumuna ayarla
-
-			// j > 0 olduğu sürece ve soldaki eleman büyükse sağa kaydır
-			while (j > 0 && dizi[j - 1] > t) {
-				dizi[j] = dizi[j - 1]; // Büyük olanı bir sağa kaydır
-				j--; // j’yi bir azalt, sola doğru git
+	
+	public void arrangamet() {
+		int t=0,i=0,j=0;
+		
+		for ( i = 0; i < array.length; i++) {
+			t=array[i];
+			
+			for ( j = i; j>0 && array[j-1]>t; j--) {
+				array[j]=array[j-1];
 			}
-
-			dizi[j] = t; // Doğru yere elemanı yerleştir
+			array[j]=t;
 		}
 	}
-
-	// Diziyi ekrana yazdırmak için kullanılan metod
-	public void yazma() {
-		System.out.println(Arrays.toString(dizi)); // Dizinin elemanlarını ekrana bastırır
+	
+	public void write() {
+		System.out.println(Arrays.toString(null));
 	}
+
 }
